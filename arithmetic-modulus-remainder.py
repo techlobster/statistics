d711d9c7e53b87a8738a2b63a3f69d15f6a26fa9
Ben Millar <91565892+techlobster@users.noreply.github.com>
2025-01-12 11:11:44 -0500

    Create arithmetic-modulus-remainder.py

---
+++ arithmetic-modulus-remainder.py
@@ -0,0 +1,11 @@
+# The % sign in python is used to find the remainder
+
+# Let's test an example when 21 is divided by 6
+
+x = 21
+x% = 6
+print(x)
+
+# Output/answer = 3 because 6 goes into 21 three times (6 x 3 = 18), what's leftover is 3 or (21 - 18 = 3)
+
+# This is for notation purposes only, not professional advice
